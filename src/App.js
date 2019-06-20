0234fc0da57bebd85808b85657db45ef65153317
AngelBiondi <48496612+AngelBiondi@users.noreply.github.com>
2019-06-19 23:35:15 -0400

    damn carousel finally working

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     <div className="App">
 
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a className="navbar-brand" >R.C. BIONDI</a>
+        <a className="navbar-brand" href='/' >R.C. BIONDI</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
--- src/mainp.js
+++ src/mainp.js
@@ -1,3 +1,6 @@
+import 'bootstrap/js/dist/util.js';
+import 'bootstrap/js/dist/dropdown';
+import 'boostrap/js/dist/carousel';
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -20,12 +23,49 @@ import { NavLink , Route , Switch, Link} from 'react-router-dom';
 
 
 function mainp() {
+    
+    const script2 = document.createElement("script");
+
+    script2.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
+    script2.async = true;
+
+    document.body.appendChild(script2);
+
+    const script = document.createElement("script");
+
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/umd/util.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+// no es eso
+
     return(
-
+        
     <div className="App">
+         <script src="/util.js"></script>
           <header className="App-header">
-        <img className="fp" src={rcb5} />
-
+          <div className="carousel slide" data-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src={rcb1} className="d-block w-100" />
+  </div>
+    <div className="carousel-item">
+      <img src={rcb2} className="d-block w-100" />
+    </div>
+    <div className="carousel-item">
+      <img src={rcb3} className="d-block w-100" />
+    </div>
+  </div>
+  <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="sr-only">Previous</span>
+  </a>
+  <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="sr-only">Next</span>
+  </a>
+</div>
       </header>
  <section className="descrip">
         <img className="sp" src={logorc} />   <h1 className='textoo'>QUIENES SOMOS</h1>  <p className="textoo"> <br />
@@ -40,7 +80,7 @@ Estamos ubicados en el corazón de la ciudad.<br /> Distribuimos dentro de todo 
       <hr />
     <section className='sections'>
  
-    <p className='texto'>
+    <div className='texto'>
       <div className='card-deck'>
         <div className="card" stylesheet="width: 18rem;">
           <img src={pana} className="card-img-top" alt="..." />
@@ -77,14 +117,14 @@ Estamos ubicados en el corazón de la ciudad.<br /> Distribuimos dentro de todo 
           </div>
         </div>
       </div>
-    </p>
+    </div>
   </section>
 
   <hr />
 
   <section className="mapouter">
     <div className="gmap_canvas">
-      <iframe width="600" height="500" className="gmap_canvas" src="https://maps.google.com/maps?q=refrigeracion%20comercial%20biondi&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" />
+      <iframe width="600" height="500" className="gmap_canvas" src="https://maps.google.com/maps?q=refrigeracion%20comercial%20biondi&t=&z=13&ie=UTF8&iwloc=&output=embed" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" />
     </div>
   </section>
 
@@ -112,7 +152,9 @@ Estamos ubicados en el corazón de la ciudad.<br /> Distribuimos dentro de todo 
       <a href="https://www.google.com/maps/place/Refrigeraci%C3%B3n+Comercial+Biondi/@10.0721212,-69.3337968,17z/data=!3m1!4b1!4m5!3m4!1s0x8e876772cdf5b0e3:0x1043b2661ce369d2!8m2!3d10.0721212!4d-69.3316081" className="btn btn-dark btn-lg active" role="button" aria-pressed="true">COMO LLEGAR</a>
     </p>
   </section>
+  <script src="/util.js"></script>
     </div>
+
     );
 }
 export default mainp;

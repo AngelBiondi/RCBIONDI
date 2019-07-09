c4884ac0762b12aa1f4f4d1e3a5c87dab3cef16d
AngelBiondi <48496612+AngelBiondi@users.noreply.github.com>
2019-07-08 22:28:48 -0400

    aa

--- src/panaderia.js
+++ src/panaderia.js
@@ -2,45 +2,36 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App'
+import five from './equipos/Panaderia/5.jpg'
+import ocho from './equipos/Panaderia/8.jpg'
+import nueve from './equipos/Panaderia/9.jpg'
+import diez from './equipos/Panaderia/10.jpg'
+import catorce from './equipos/Panaderia/14.jpg'
+import sixteen from './equipos/Panaderia/16.jpg'
+import twenty  from './equipos/Panaderia/20.jpg'
+import twentyfour from './equipos/Panaderia/24.jpg'
+import twentyfive  from './equipos/Panaderia/25.jpg'
+import veintiseis from './equipos/Panaderia/26.jpg'
+import veintisiete from './equipos/Panaderia/27.jpg'
+import thirtyone from './equipos/Panaderia/31.jpg'
 
 function panaderia() {
 
   return (
     <div className='App'>
       <h1>Equipos para Panaderias</h1>
-     <div className="listish">
-
-     <ul className='list-group list-group-flush'>
-
-        <h2>Mobiliario</h2>
-        
-        <li className='list-group-item'>Mostrador pasta seca </li>
-        <li className='list-group-item'>Mostrador charcutero </li>
-        <li className='list-group-item'>Mostrador pasta fría </li>
-        <li className='list-group-item'>Retromostrador</li>
-        <li className='list-group-item'>Retromostrador panero</li>
-        <li className='list-group-item'>Vitrina refrigerada</li>
-        <li className='list-group-item'>Mueble para máquina de café</li>
-        <li className='list-group-item'>Mueble de caja</li>
-        <li className='list-group-item'>Máquina de café express</li>
-        <li className='list-group-item'>Molino para café</li>
-        <li className='list-group-item'>Rebanadora</li>
-        <li className='list-group-item'>Termoselladora</li>
-        <li className='list-group-item'>Balanza electrónica</li>
-        <li className='list-group-item'>Balanza electrónica con etiquetadora</li>
-     </ul>
-     
-     <ul className='list-group list-group-flush'>
-        <h2>Maquinaria</h2>
-        <li className='list-group-item'>Amasadora </li>
-        <li className='list-group-item'>Sobadora </li>
-        <li className='list-group-item'>Batidora</li>
-        <li className='list-group-item'>Laminadora</li>
-        <li className='list-group-item'>Horno para pan </li>
-        <li className='list-group-item'>Cava sartenera</li>
-        <li className='list-group-item'>Cava cuarto</li>
-     </ul>
-     </div>
+    <img className='panimg' src={five}/>
+    <img className='panimg' src={ocho}/>
+    <img className='panimg' src={nueve}/>
+    <img className='panimg' src={diez}/>
+    <img className='panimg' src={catorce}/>
+    <img className='panimg' src={sixteen}/>
+    <img className='panimg' src={twenty}/>
+    <img className='panimg' src={twentyfour}/>
+    <img className='panimg' src={twentyfive}/>
+    <img className='panimg' src={veintiseis}/>
+    <img className='panimg' src={veintisiete}/>
+    <img className='panimg' src={thirtyone}/>
     </div>
   );
 

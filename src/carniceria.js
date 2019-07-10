c584a4a6454a391f2afbe53e369a5b159781a02c
AngelBiondi <48496612+AngelBiondi@users.noreply.github.com>
2019-07-09 22:41:29 -0400

    oo

--- src/carniceria.js
+++ src/carniceria.js
@@ -7,22 +7,11 @@ import App from './App'
   
         return (
 <div>
-<ul className='list-group list-group-flush'>
-        <h2>Equipos para Carniceria</h2>
-        <li className='list-group-item'>Mostrador carnicero</li>
-        <li className='list-group-item'>Molino para carne </li>
-        <li className='list-group-item'>Vitrina refrigerada </li>
-        <li className='list-group-item'>Molino raya queso </li>
-        <li className='list-group-item'>Termoselladora </li>
-        <li className='list-group-item'>Rebanadora</li>
-        <li className='list-group-item'>Cava cuarto </li>
-        <li className='list-group-item'>Fregaderos industriales </li>
-        <li className='list-group-item'>Congelador horizontal</li>
-        <li className='list-group-item'>Molinos para carne </li>
-        <li className='list-group-item'>Molinos para queso </li>
-        <li className='list-group-item'>Mesas de trabajo en a/inoxidable</li>
 
-</ul>
+        <h2>Equipos para Carniceria</h2>
+       
+
+
 
 </div>
         );

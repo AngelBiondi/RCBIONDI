91ed278b9a7ebb1f7e55bcedf7bdad234bcba05c
AngelBiondi <48496612+AngelBiondi@users.noreply.github.com>
2019-07-12 20:30:37 -0400

    dddddddd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import rcb1 from './rcb1.png'
@@ -12,73 +13,75 @@ import charcuteria from './charcuteria'
 import carniceria from './carniceria'
 import restaurante from './restaurante'
 import mainp from './mainp'
-import contactanos from './contact' 
-import { NavLink , Route , Switch, Link} from 'react-router-dom';
+import contactanos from './contact'
+import { NavLink, Route, Switch, Link } from 'react-router-dom';
 
 
 
 function App() {
-  constructor(props); {
-    super(props);
-    this.state = { apiResponse: '' };
-}
+  constructor() {
+    super()
+    this.state = { apiResponse: '' }
+  }
 
-callAPI(); {
+  callAPI() {
     fetch('http://localhost:9000/testAPI')
-        .then(res => res.text())
-        .then(res => this.setState({ apiResponse: res }));
-}
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
 
-componentWillMount(); {
+  componentWillMount() {
     this.callAPI();
-}
-  return (
-    <div className="App">
- 
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a className="navbar-brand" href='/' >R.C. BIONDI</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">   
-              <a className="nav-link" href="/">Inicio <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/contact">Contacto</a>
-             
-            </li>
-            {/* <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li> */}
-          </ul>
-          <span className="navbar-text">
-            Refrigeracion Comercial Biondi
-    </span>
-        </div>
-      </nav>
-    
-     
+  }
+  render() {
+    return (
+      <div className="App">
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+          <a className="navbar-brand" href='/' >R.C. BIONDI</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">Inicio <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/contact">Contacto</a>
+
+              </li>
+              {/* <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li> */}
+            </ul>
+            <span className="navbar-text">
+              Refrigeracion Comercial Biondi
+      </span>
+          </div>
+        </nav>
+
+
 
         <Switch>
-        <Route path="/" exact component={mainp} />
-  <Route exact path="/panaderia"  component={panaderia}/>
-   <Route exact path="/contact"  component={contactanos}/>
-  <Route exact path="/charcuteria"  component={charcuteria}/>
-  <Route exact path="/restaurante"  component={restaurante}/>
-  <Route exact path="/carniceria"  component={carniceria}/>
- </Switch>
-     
-     
+          <Route path="/" exact component={mainp} />
+          <Route exact path="/panaderia" component={panaderia} />
+          <Route exact path="/contact" component={contactanos} />
+          <Route exact path="/charcuteria" component={charcuteria} />
+          <Route exact path="/restaurante" component={restaurante} />
+          <Route exact path="/carniceria" component={carniceria} />
+        </Switch>
 
-      <footer className="footer footer-expand-lg navbar-dark bg-primary">
-        <p>REFRIGERACION COMERCIAL BIONDI </p>
-      </footer>
-     
-      <p className="App-intro">;{this.state.apiResponse}</p>
-    </div>
-  );
+
+
+        <footer className="footer footer-expand-lg navbar-dark bg-primary">
+          <p>REFRIGERACION COMERCIAL BIONDI </p>
+        </footer>
+
+        <p className="App-intro">;{this.state.apiResponse}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
